164f064f7d97e60002e214b5bfc6550b27ddbcc9
Paul Jones <paul.tyler.jones@gmail.com>
2024-09-27 08:11:49 +0000

    Adds try-exception handler

--- MrJones_Sprint_Projectv2.0/MrJones_Sprint_Projectv2.0.py
+++ MrJones_Sprint_Projectv2.0/MrJones_Sprint_Projectv2.0.py
@@ -1,5 +1,12 @@
-input('What is your age?: ')
-print()
+try:
+    input('What is your age?: ')
+    print()
 
-dog = input ('What is your age?: ')
-print (f'{dog}?! That is a great age to be!')
+    dog = input ('What is your age?: ')
+    print (f'{dog}?! That is a great age to be!')
+
+    dog = int(input ('What is your age?: '))
+    print (f'{dog}?! That is a great age to be!')
+
+except ValueError:
+    print('Error: That is not a valid number.')
